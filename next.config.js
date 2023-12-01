62476878de23f156be39db8dc2149f0b90a67aa4
Savalaram Redkar <rredkar1996@gmail.com>
2023-12-01 23:56:15 +0530

    added deploy job in workflow

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  // Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  trailingSlash: true,
+  // Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  skipTrailingSlashRedirect: true,
+  // Change the output directory `out` -> `dist`
+  distDir: "dist",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
